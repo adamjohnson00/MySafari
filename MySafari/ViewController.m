4a36a165368d27edbb0da439810c2336169d5fcb
DavidSights <davidseitzjr@gmail.com>
2015-05-13 16:25:28 -0500

    added back/forward/refresh/stop buttons along with methods

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -13,7 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
-
+@property (weak, nonatomic) IBOutlet UIButton *goBackButton;
 
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 
@@ -23,27 +23,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-
+    self.goBackButton.enabled = NO;
+    [self goTo:@"http://google.com"];
 
 }
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    NSURL *url = [NSURL URLWithString:textField.text];
+- (void) goTo:(NSString *) text{
+    NSURL *url = [NSURL URLWithString:text];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self goTo:textField.text];
+    [textField resignFirstResponder];
+    self.goBackButton.enabled = YES;
     NSLog(@"Return was hit");
 
     return YES;
 }
-
+- (IBAction)onBackButtonPressed:(id)sender {
+    [self.webView goBack];
+    if (self.webView.canGoBack == NO) {
+        self.goBackButton.enabled = NO;
+    }
+}
+- (IBAction)onReloadButtonPressed:(id)sender {
+    [self.webView reload];
+}
 
 -(void)webViewDidStartLoad:(UIWebView *)webView
 {
     [self.spinner startAnimating];
 }
+- (IBAction)onStopButtonLoadngPressed:(id)sender {
+    [self.webView stopLoading];
+}
 
+- (IBAction)onForwardButtonPressed:(id)sender {
+    if (self.webView.canGoForward == YES) {
+        [self.webView goForward];
+    } else {
+        self.goBackButton.enabled = NO;
+    }
+    
+}
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView
 {
